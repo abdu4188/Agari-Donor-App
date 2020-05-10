c4bd4e9f27dff4b180f675f654afe05aef2c407e
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-05-10 15:27:50 +0300

    remove pageview widgets

--- lib/screens/donation_history.dart
+++ lib/screens/donation_history.dart
@@ -12,120 +12,116 @@ class DonationHistoryState extends State<DonationHistory>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: PageView(
-        children: <Widget>[
-         Column(
-           children: <Widget>[
-             Expanded(
-                child: Stack(
-                 children: <Widget>[
-                   ClipRRect(
-                     borderRadius: BorderRadius.only(
-                       bottomLeft: Radius.circular(20),
-                       bottomRight: Radius.circular(20)
-                     ),
-                    child: Container(
-                       height: 280,
-                       width: 320,
-                       decoration: BoxDecoration(
-                         gradient: LinearGradient(
-                           begin: Alignment.topLeft,
-                           end: Alignment.bottomRight,
-                           colors: [Color(int.parse('0xff75c8b7')), Color(int.parse('0xff3fa1a9')), Color(int.parse('0xff6cbbab'))]
-                         ),
-                       ),
-                       child: Padding(
-                         padding: EdgeInsets.only(top: 100, left: 180),
-                         child: Column(
-                           children: <Widget>[
-                             Text(
-                              "Donation",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.w600
-                              ),
-                            ),
-                            Text(
-                              "History",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.w600
-                              ),
-                            ),
-                           ],
-                         )
-                       ),
+     body: Column(
+       children: <Widget>[
+         Expanded(
+            child: Stack(
+             children: <Widget>[
+               ClipRRect(
+                 borderRadius: BorderRadius.only(
+                   bottomLeft: Radius.circular(20),
+                   bottomRight: Radius.circular(20)
+                 ),
+                child: Container(
+                   height: 280,
+                   width: 320,
+                   decoration: BoxDecoration(
+                     gradient: LinearGradient(
+                       begin: Alignment.topLeft,
+                       end: Alignment.bottomRight,
+                       colors: [Color(int.parse('0xff75c8b7')), Color(int.parse('0xff3fa1a9')), Color(int.parse('0xff6cbbab'))]
                      ),
                    ),
-                   Positioned(
-                     top: 200,
-                     right: 0,
-                     left: 0,
-                     bottom: 0,
-                     child: ListView.builder(
-                       itemCount: 16,
-                       itemBuilder: (BuildContext context, index){
-                         return Card(
-                           shape: RoundedRectangleBorder(
-                             borderRadius: BorderRadius.circular(18)
-                           ),
-                           child: Row(
-                             children: <Widget>[
-                               Padding(
-                                 padding: const EdgeInsets.only(left: 20, bottom: 20, top: 15),
-                                 child: Column(
-                                   children: <Widget>[
-                                     Text(
-                                       "Standard",
-                                       style: TextStyle(
-                                         fontSize: 25,
-                                         fontWeight: FontWeight.w600,
-                                         color: Color(int.parse('0xff3fa1a9'))
-                                       ),
-                                      ),
-                                     SizedBox(
-                                       height: 10,
-                                     ),
-                                     ClipRRect(
-                                       borderRadius: BorderRadius.circular(20),
-                                       child: Container(
-                                         color:  Color(int.parse('0xff3fa1a9')),
-                                         child: Padding(
-                                           padding: const EdgeInsets.all(8.0),
-                                           child: Text(
-                                             "Pending",
-                                             style: TextStyle(
-                                               color: Colors.white
-                                             ),
-                                          ),
-                                         ),
-                                       ),
-                                     )
-                                   ],
-                                 ),
-                               ),
-                               Padding(
-                                 padding: const EdgeInsets.only(left: 65),
-                                 child: Text(
-                                   "10 wed 2020",
-                                   style: TextStyle(
-                                     color: Color(int.parse('0xff3fa1a9')),
-                                     fontSize: 20
-                                   ),
-                                ),
-                               )
-                             ],
-                           )
-                         );
-                       },
-                     ),
-                   )
-                 ],
+                   child: Padding(
+                     padding: EdgeInsets.only(top: 100, left: 180),
+                     child: Column(
+                       children: <Widget>[
+                         Text(
+                          "Donation",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                        Text(
+                          "History",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                       ],
+                     )
+                   ),
+                 ),
                ),
-             ),
-           ],
+               Positioned(
+                 top: 200,
+                 right: 0,
+                 left: 0,
+                 bottom: 0,
+                 child: ListView.builder(
+                   itemCount: 16,
+                   itemBuilder: (BuildContext context, index){
+                     return Card(
+                       shape: RoundedRectangleBorder(
+                         borderRadius: BorderRadius.circular(18)
+                       ),
+                       child: Row(
+                         children: <Widget>[
+                           Padding(
+                             padding: const EdgeInsets.only(left: 20, bottom: 20, top: 15),
+                             child: Column(
+                               children: <Widget>[
+                                 Text(
+                                   "Standard",
+                                   style: TextStyle(
+                                     fontSize: 25,
+                                     fontWeight: FontWeight.w600,
+                                     color: Color(int.parse('0xff3fa1a9'))
+                                   ),
+                                  ),
+                                 SizedBox(
+                                   height: 10,
+                                 ),
+                                 ClipRRect(
+                                   borderRadius: BorderRadius.circular(20),
+                                   child: Container(
+                                     color:  Color(int.parse('0xff3fa1a9')),
+                                     child: Padding(
+                                       padding: const EdgeInsets.all(8.0),
+                                       child: Text(
+                                         "Pending",
+                                         style: TextStyle(
+                                           color: Colors.white
+                                         ),
+                                      ),
+                                     ),
+                                   ),
+                                 )
+                               ],
+                             ),
+                           ),
+                           Padding(
+                             padding: const EdgeInsets.only(left: 65),
+                             child: Text(
+                               "10 wed 2020",
+                               style: TextStyle(
+                                 color: Color(int.parse('0xff3fa1a9')),
+                                 fontSize: 20
+                               ),
+                            ),
+                           )
+                         ],
+                       )
+                     );
+                   },
+                 ),
+               )
+             ],
+           ),
          ),
        ],
      ),
--- lib/screens/payment_option.dart
+++ lib/screens/payment_option.dart
@@ -22,175 +22,171 @@ class PaymentOptionState extends State<PaymentOption> with TickerProviderStateMi
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNav(3),
-      body: PageView(
-        children: <Widget>[
-          SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(right: 240, top: 30,),
-                  child: GestureDetector(
-                    child: Icon(
-                      Icons.arrow_back,
-                      size: 35,
-                      color: Color(int.parse('0xff00838f')),
-                    ),
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 240, top: 30,),
+              child: GestureDetector(
+                child: Icon(
+                  Icons.arrow_back,
+                  size: 35,
+                  color: Color(int.parse('0xff00838f')),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 0, top: 25),
-                  child: Text(
-                    "Payment Options",
-                    style: TextStyle(
-                      color: Color(int.parse('0xff00838f')),
-                      fontSize: 25,
-                      fontWeight: FontWeight.w700
-                    ), 
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: TabBar(
-                    controller: _tabController,
-                    labelColor: Color(int.parse('0xff00838f')),
-                   tabs: <Widget>[
-                     Tab(
-                       text: "USSD",
-                     ),
-                     Tab(
-                       text: "Mobile Banking",
-                     ),
-                     Tab(
-                       text: "Deposit",
-                     ),
-                   ], 
-                  ),
-                ),
-                Container(
-                  height: 290,
-                  margin: EdgeInsets.only(left: 16.0, right: 16.0),
-                  child: TabBarView(
-                    controller: _tabController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 0, top: 25),
+              child: Text(
+                "Payment Options",
+                style: TextStyle(
+                  color: Color(int.parse('0xff00838f')),
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700
+                ), 
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: TabBar(
+                controller: _tabController,
+                labelColor: Color(int.parse('0xff00838f')),
+               tabs: <Widget>[
+                 Tab(
+                   text: "USSD",
+                 ),
+                 Tab(
+                   text: "Mobile Banking",
+                 ),
+                 Tab(
+                   text: "Deposit",
+                 ),
+               ], 
+              ),
+            ),
+            Container(
+              height: 290,
+              margin: EdgeInsets.only(left: 16.0, right: 16.0),
+              child: TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  ListView(
                     children: <Widget>[
-                      ListView(
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () => {
-                              showDialog(
-                                context: context,
-                                child: PinDialog()
-                              )
-                            },
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(18),
-                              child: Card(
-                                color: Color(int.parse('0xff6cbbab')),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    children: <Widget>[
-                                      Image.asset(
-                                        'assets/images/cbe.png',
-                                        width: 35,
-                                        height: 35,
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Text(
-                                        "Donate 500 birr using CBE",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Color(int.parse('0xff00838f')),
-                                        ),
-                                      )
-                                    ],
+                      GestureDetector(
+                        onTap: () => {
+                          showDialog(
+                            context: context,
+                            child: PinDialog()
+                          )
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(18),
+                          child: Card(
+                            color: Color(int.parse('0xff6cbbab')),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Image.asset(
+                                    'assets/images/cbe.png',
+                                    width: 35,
+                                    height: 35,
                                   ),
-                                ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    "Donate 500 birr using CBE",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Color(int.parse('0xff00838f')),
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
                           ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(18),
-                            child: Card(
-                              color: Color(int.parse('0xff6cbbab')),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: <Widget>[
-                                    Image.asset(
-                                      'assets/images/amole.jpg',
-                                      width: 30,
-                                      height: 30,
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Text(
-                                      "Donate 500 birr using AMOLE",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: Color(int.parse('0xff00838f')),
-                                      ),
-                                    )
-                                  ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(18),
+                        child: Card(
+                          color: Color(int.parse('0xff6cbbab')),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: <Widget>[
+                                Image.asset(
+                                  'assets/images/amole.jpg',
+                                  width: 30,
+                                  height: 30,
                                 ),
-                              ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Donate 500 birr using AMOLE",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(int.parse('0xff00838f')),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(18),
-                            child: Card(
-                              color: Color(int.parse('0xff6cbbab')),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: <Widget>[
-                                    Image.asset(
-                                      'assets/images/hellocash.jpg',
-                                      width: 35,
-                                      height: 35,
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Text(
-                                      "Donate 500 birr using HELLO CASH",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Color(int.parse('0xff00838f')),
-                                      ),
-                                    )
-                                  ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(18),
+                        child: Card(
+                          color: Color(int.parse('0xff6cbbab')),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: <Widget>[
+                                Image.asset(
+                                  'assets/images/hellocash.jpg',
+                                  width: 35,
+                                  height: 35,
                                 ),
-                              ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Donate 500 birr using HELLO CASH",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color(int.parse('0xff00838f')),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                        ]
+                        ),
                       ),
-                      ListView(
-                        children: <Widget>[
-                          Text("Mobile Banking")
-                        ],
-                      ),
-                      ListView(
-                        children: <Widget>[
-                          Text("Deposit")
-                        ],
-                      ),
+                    ]
+                  ),
+                  ListView(
+                    children: <Widget>[
+                      Text("Mobile Banking")
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
-        ],
+                  ListView(
+                    children: <Widget>[
+                      Text("Deposit")
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
